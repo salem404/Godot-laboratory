73c06ea862473d913b9d0a7005c35bd84a9c8bb7
salem404 <prumeu@protonmail.com>
2024-10-27 20:24:41 +0100

    shitty base

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,32 +1,36 @@
 extends CharacterBody2D
 
-
-const SPEED: float = 130.0
-const JUMP_VELOCITY = -300.0
-
-@onready var animation_tree: AnimationTree = $AnimationTree
-
-@onready var state_machine = animation_tree.get("parameters/playback")
+@export var speed = 120 # How fast the player will move (pixels/sec).
 
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
 	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		
 
-	if direction:
-		velocity.x = direction * SPEED
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.flip_h = velocity.x > 0
+		if velocity.x != 0:
+			$AnimatedSprite2D.animation = "walk_side"
+		else:
+			if velocity.y > 0:
+				$AnimatedSprite2D.animation = "walk_front"
+			else:
+				$AnimatedSprite2D.animation = "walk_back"
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		$AnimatedSprite2D.animation = "idle_front"
+	
+	$AnimatedSprite2D.play()
+	position += velocity * delta
 	move_and_slide()
+	
