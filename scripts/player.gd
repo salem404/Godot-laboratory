29dbf9dddae0d1d63b80bd590e489e50853cf4ae
salem404 <prumeu@protonmail.com>
2024-10-28 21:02:17 +0100

    player state tree moves

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,27 +1,9 @@
 class_name Player
 extends CharacterBody2D
 
-@export var speed = 120 # How fast the player will move (pixels/sec).
+var aim_position : Vector2 = Vector2(1, 0)
 
-
-func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO
-	
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-		
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		
-
-	
-	position += velocity * delta
-	move_and_slide()
-	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var half_viewport = get_viewport_rect().size / 2.0
+		aim_position = (event.position - half_viewport)
---
+++ scripts/player_movement.gd
@@ -0,0 +1,33 @@
+extends Node
+
+##########################################################
+# This is the player's movement controller.
+# Instead of placing all of the movement stuff inside
+# of the player, we move the code to a separate component
+##########################################################
+
+@export_group("Speed Values")
+@export var max_speed := 50.0
+@export var acceleration_time := 0.1
+
+@onready var player : CharacterBody2D = get_owner()
+
+
+func _physics_process(delta):
+	# Read the player's current velocity
+	var velocity = player.velocity
+	
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	# Apply any changes to velocity
+	velocity = velocity.move_toward(input_direction*max_speed, (1.0 / acceleration_time) * delta * max_speed)
+	
+	if input_direction.y && sign(input_direction.y) != sign(velocity.y):
+		velocity.y *= 0.75 
+	
+	if input_direction.x && sign(input_direction.x) != sign(velocity.x):
+		velocity.x *= 0.75
+	
+	# Reassign velocity and move the player
+	player.velocity = velocity
+	player.move_and_slide()
